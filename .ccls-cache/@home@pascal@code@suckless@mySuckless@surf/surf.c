d45f551cfa07602d597839697e4cc59ea791cdac
pascalbakker <pascalsbakker@gmail.com>
2021-03-04 09:58:41 -0500

    cleaning up

--- .ccls-cache/@home@pascal@code@suckless@mySuckless@surf/surf.c
+++ .ccls-cache/@home@pascal@code@suckless@mySuckless@surf/surf.c
@@ -1465,7 +1465,7 @@ void myCSS(void){
     screen = gdk_display_get_default_screen (display);
     gtk_style_context_add_provider_for_screen (screen, GTK_STYLE_PROVIDER (provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
 
-    const gchar *myCssFile = "~/.surf/styles/mystyle.css";
+    const gchar *myCssFile = "mystyle.css";
     GError *error = 0;
 
     gtk_css_provider_load_from_file(provider, g_file_new_for_path(myCssFile), &error);
